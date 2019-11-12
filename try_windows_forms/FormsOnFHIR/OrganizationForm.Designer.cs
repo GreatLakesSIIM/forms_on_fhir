227474967f8db5aa3917c2440ebe54551ef06ab5
Peter Dobbs <dobbsp8@gmail.com>
2019-11-12 15:51:42 -0600

    adding org entry form

---
+++ try_windows_forms/FormsOnFHIR/OrganizationForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FormsOnFHIR {
+  partial class OrganizationForm {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.orgNameEntry = new System.Windows.Forms.TextBox();
+      this.orgNameLabel = new System.Windows.Forms.Label();
+      this.orgTypeLabel = new System.Windows.Forms.Label();
+      this.orgTypeSelector = new System.Windows.Forms.ComboBox();
+      this.orgFormLabel = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // orgNameEntry
+      // 
+      this.orgNameEntry.Location = new System.Drawing.Point(123, 56);
+      this.orgNameEntry.Name = "orgNameEntry";
+      this.orgNameEntry.Size = new System.Drawing.Size(118, 20);
+      this.orgNameEntry.TabIndex = 0;
+      // 
+      // orgNameLabel
+      // 
+      this.orgNameLabel.AutoSize = true;
+      this.orgNameLabel.Location = new System.Drawing.Point(17, 59);
+      this.orgNameLabel.Name = "orgNameLabel";
+      this.orgNameLabel.Size = new System.Drawing.Size(100, 13);
+      this.orgNameLabel.TabIndex = 1;
+      this.orgNameLabel.Text = "Organization Name:";
+      // 
+      // orgTypeLabel
+      // 
+      this.orgTypeLabel.AutoSize = true;
+      this.orgTypeLabel.Location = new System.Drawing.Point(17, 95);
+      this.orgTypeLabel.Name = "orgTypeLabel";
+      this.orgTypeLabel.Size = new System.Drawing.Size(96, 13);
+      this.orgTypeLabel.TabIndex = 3;
+      this.orgTypeLabel.Text = "Organization Type:";
+      // 
+      // orgTypeSelector
+      // 
+      this.orgTypeSelector.FormattingEnabled = true;
+      this.orgTypeSelector.Location = new System.Drawing.Point(120, 95);
+      this.orgTypeSelector.Name = "orgTypeSelector";
+      this.orgTypeSelector.Size = new System.Drawing.Size(121, 21);
+      this.orgTypeSelector.TabIndex = 4;
+      // 
+      // orgFormLabel
+      // 
+      this.orgFormLabel.AutoSize = true;
+      this.orgFormLabel.Location = new System.Drawing.Point(17, 18);
+      this.orgFormLabel.Name = "orgFormLabel";
+      this.orgFormLabel.Size = new System.Drawing.Size(89, 13);
+      this.orgFormLabel.TabIndex = 5;
+      this.orgFormLabel.Text = "ORGANIZATION";
+      // 
+      // OrganizationForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.orgFormLabel);
+      this.Controls.Add(this.orgTypeSelector);
+      this.Controls.Add(this.orgTypeLabel);
+      this.Controls.Add(this.orgNameLabel);
+      this.Controls.Add(this.orgNameEntry);
+      this.Name = "OrganizationForm";
+      this.Size = new System.Drawing.Size(506, 335);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox orgNameEntry;
+    private System.Windows.Forms.Label orgNameLabel;
+    private System.Windows.Forms.Label orgTypeLabel;
+    private System.Windows.Forms.ComboBox orgTypeSelector;
+    private System.Windows.Forms.Label orgFormLabel;
+  }
+}
---
+++ try_windows_forms/FormsOnFHIR/OrganizationForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Specification;
+
+namespace FormsOnFHIR {
+  public partial class OrganizationForm : UserControl, IAdministrativeFhirResource {
+    public OrganizationForm() {
+      InitializeComponent();
+      var types = new[] { "" };
+
+      orgTypeSelector.DataSource = types;
+    }
+
+    public void EnterInfo() {
+      throw new NotImplementedException();
+    }
+
+    public void Search() {
+      throw new NotImplementedException();
+    }
+  }
+}
